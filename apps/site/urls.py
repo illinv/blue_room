b37f7b4fd4eedb1cf096720c020f5a41b57671e7
ilya.ionov <ilya.ionov@simbirsoft.com>
2018-04-24 16:27:06 +0400

    добавлена визуализация фичи

--- apps/site/urls.py
+++ apps/site/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
+from .views import ProjectListView, ProjectDetailView, FeatureDetail
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
-
+    path('feature/<int:pk>/', FeatureDetail.as_view(), name='feature-detail'),
 ]
--- apps/site/views.py
+++ apps/site/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
-from core.models import Project
+from core.models import Project, Feature, Bug, TestCase
 
 
 class ProjectListView(ListView):
@@ -16,3 +16,16 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = 'project-detail.html'
 
+
+class FeatureDetail(DetailView):
+    model = Feature
+    template_name = 'feature-detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        feature = context['object']
+        bugs = Bug.objects.filter(feature=feature)
+        test_cases = TestCase.objects.filter(feature=feature)
+        context['bugs'] = bugs
+        context['test_cases'] = test_cases
+        return context
